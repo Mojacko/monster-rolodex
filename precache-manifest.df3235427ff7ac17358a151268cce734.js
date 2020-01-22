94ae9bbc7ba4be8476a22be8c4328a06bdc3f7a0
rans <rans>
2020-01-22 20:10:20 +0800

    Updates

---
+++ precache-manifest.df3235427ff7ac17358a151268cce734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3ab124fce429dae107e370ff98baae",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c990e90211ed93eae6b1",
+    "url": "/monster-rolodex/static/css/main.b52abc97.chunk.css"
+  },
+  {
+    "revision": "4d04555662cc842eb40b",
+    "url": "/monster-rolodex/static/js/2.7e398354.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7e398354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c990e90211ed93eae6b1",
+    "url": "/monster-rolodex/static/js/main.ef119066.chunk.js"
+  },
+  {
+    "revision": "4cf4f8a7fe2234154a3a",
+    "url": "/monster-rolodex/static/js/runtime-main.11452f00.js"
+  }
+]);
